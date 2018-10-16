00cdb93527caa4461bd3826e1ec58c4badce4719
aristides1000 <aristides1000@gmail.com>
2018-10-16 17:31:03 -0400

    cambios localhost:9090
    se cambiaron las configuraciones del archivo config.inc.php

--- app/config.inc.php
+++ app/config.inc.php
@@ -9,7 +9,7 @@ define('NOMBRE_BD', 'blog');
 //importante incluir las nuevas rutas aquí, esto incluye las nuevas de carrito de compra
 //ESTAS SON LAS RUTAS DE MI PÁGINA
 //PARA ACTIVARLAS EN LOS VINCULOS DEBO ESCRIBIR "href="<?php echo RUTA_ENTRADAS(aqui va el cierre de la etiqueta de llamado a php)""
-define("SERVIDOR", "http://localhost:9090/Proyecto");/*mi ruta a la carpeta proyecto, tengo que estar muy pilas con esto al momento de montar mi carpeta en otra compu OJO OJO OJO OJO OJO OJO OJO sobre todo con el puerto de localhost que por defecto es 80, eso me va generar un error si no estoy pilas OJO*/
+define("SERVIDOR", "http://localhost/Proyecto");/*mi ruta a la carpeta proyecto, tengo que estar muy pilas con esto al momento de montar mi carpeta en otra compu OJO OJO OJO OJO OJO OJO OJO sobre todo con el puerto de localhost que por defecto es 80, eso me va generar un error si no estoy pilas OJO*/
 define("RUTA_REGISTRO", SERVIDOR."/registro");
 define("RUTA_REGISTRO_CORRECTO", SERVIDOR."/registro-correcto");
 define("RUTA_LOGIN", SERVIDOR."/login");
